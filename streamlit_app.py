b0aee6f63729823215d3371e60e9e6270aa57999
psudhakar <sudhakar.parsi@gmail.com>
2023-11-03 19:41:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,9 @@
 import streamlit as st
-from email_validator import validate_email, EmailNotValidError
 
 st.title("NCHS After Prom Fundraiser")
+st.markdown("<h1 style='text-align: center;'>NCHS After Prom Fundraiser</h1>", unsafe_allow_html=True)
 st.markdown("Once an order is submitted, the digital coupon will be delivered in 24-48 hours to your email address given below.")
 
-quantity = st.selectbox("Select quantity", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], index=0)
+quantity = st.selectbox("Select quantity", list(range(1, 11)), index=0)
 
 email = st.text_input("Email Address")
-
-if st.button("Submit"):
-    try:
-        valid = validate_email(email)
-        st.success("Form submitted!")
-    except EmailNotValidError:
-        st.error("Please enter a valid email address")
