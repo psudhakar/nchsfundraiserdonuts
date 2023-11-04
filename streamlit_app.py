c3d98171cfa92886f56689eaa6b149c217e10055
psudhakar <sudhakar.parsi@gmail.com>
2023-11-03 19:50:36 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,20 @@
 import streamlit as st
 
 st.title("NCHS After Prom Fundraiser")
-st.markdown("<h1 style='text-align: center;'>NCHS After Prom Fundraiser</h1>", unsafe_allow_html=True)
 st.markdown("Once an order is submitted, the digital coupon will be delivered in 24-48 hours to your email address given below.")
 
-quantity = st.selectbox("Select quantity", list(range(1, 11)), index=0)
+quantity = st.selectbox("Select quantity", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], index=0)
 
 email = st.text_input("Email Address")
 
 st.header("Please send your payments to:")
-payment_table = [
-    ["Zelle", "sudhakar.parsi@gmail.com"],
-    ["Venmo", "lkjdalsd"],
-    ["Cash app", "ajdlkada"]
-]
 
-st.table(payment_table)
+payment_methods = [["Payment Method", "ID"],
+                   ["Zelle", "sudhakar.parsi@gmail.com"],
+                   ["Venmo", "lkjdalsd"], 
+                   ["Cash app", "ajdlkada"]]
+
+st.table(payment_methods)
+
+if st.button("Submit"):
+    st.success("Form submitted!")
