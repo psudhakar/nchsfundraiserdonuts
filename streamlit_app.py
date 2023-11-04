aa1312c95a859b0e0ecf0ddc18ad8fa6c7351e9e
psudhakar <sudhakar.parsi@gmail.com>
2023-11-03 19:32:04 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,34 @@
+Python
 import streamlit as st
 
-# Set the heading
-st.header("NCHS After Prom Fundraiser")
+# Set the heading to be centered on the screen
+st.title("NCHS After Prom Fundraiser", align="center")
 
 # Add a text field
 st.write("Once an order is submitted, the digital coupon will be delivered in 24-48 hours to your email address given below.")
 
-# Add a dropdown menu
-options = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-quantity = st.selectbox("Quantity", options)
+# Add a dropdown
+dropdown_options = [str(i) for i in range(1, 11)]
+default_value = "1"
+dropdown_value = st.selectbox("Select the number of tickets you want:", dropdown_options, index=dropdown_options.index(default_value))
 
-# Add a text field for the email address
+# Add an email field
 email_address = st.text_input("Email Address")
+
+# Validate the email address format before submitting the form
+def is_valid_email(email_address):
+  import re
+  email_regex = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
+  if re.fullmatch(email_regex, email_address):
+    return True
+  else:
+    return False
 
 # Submit the form
 if st.button("Submit"):
-    # Send an email with the digital coupon
+  # Check if the email address is valid
+  if not is_valid_email(email_address):
+    st.error("Please enter a valid email address.")
+  else:
+    # Submit the form data
     # ...
-    st.success("Your order has been submitted!")
