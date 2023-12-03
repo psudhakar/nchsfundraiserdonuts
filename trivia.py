9fd92fa7246087b3b39be0d2fd42bda527b5d1d8
psudhakar <sudhakar.parsi@gmail.com>
2023-12-03 12:26:23 -0600

    Update trivia.py

--- trivia.py
+++ trivia.py
@@ -3,6 +3,33 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import pandas as pd
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+# Function to save data to Google Sheets
+def save_to_sheet(data, sheet_url):
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+
+    # Use Streamlit secrets to load credentials
+    creds_dict = {
+        'type': st.secrets['gcp_service_account']['type'],
+        'project_id': st.secrets['gcp_service_account']['project_id'],
+        'private_key_id': st.secrets['gcp_service_account']['private_key_id'],
+        'private_key': st.secrets['gcp_service_account']['private_key'],
+        'client_email': st.secrets['gcp_service_account']['client_email'],
+        'client_id': st.secrets['gcp_service_account']['client_id'],
+        'auth_uri': st.secrets['gcp_service_account']['auth_uri'],
+        'token_uri': st.secrets['gcp_service_account']['token_uri'],
+        'auth_provider_x509_cert_url': st.secrets['gcp_service_account']['auth_provider_x509_cert_url'],
+        'client_x509_cert_url': st.secrets['gcp_service_account']['client_x509_cert_url']
+    }
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+    client = gspread.authorize(creds)
+    sheet = client.open_by_url(sheet_url).sheet1
+
+    # Append data to the sheet
+    sheet.append_row(data)
 
 # Function to send email
 def send_email(to_email, data, cc_email="nchsjr.board@gmail.com"):
@@ -30,7 +57,7 @@ def send_email(to_email, data, cc_email="nchsjr.board@gmail.com"):
     server.quit()
 
 # Hide hamburger menu and footer
-st.set_page_config(page_title="Trivia Night by NCHS After Prom Team", layout="wide", menu_items={
+st.set_page_config(page_title="Trivia Night by NCHS After Prom Team", layout="centered", menu_items={
     'Get Help': None,
     'Report a bug': None,
     'About': None
@@ -42,16 +69,17 @@ st.set_page_config(page_title="Trivia Night by NCHS After Prom Team", layout="wi
 #logo_url = "https://drive.google.com/uc?id=1mTbNpv2DyU2zv8xm73wBc1GVH2EJYdqR"
 logo_url = "https://drive.google.com/uc?id=1SfAQ50kW6gybO-Pqcs6mFWQ2DiLY0mcH"
 
-
-# Display the logo
-st.image(logo_url)
+# Centering the logo at the top of the screen
+col1, col2, col3 = st.columns([1,2,1])
+with col2:
+    st.image(logo_url,use_column_width=True)
 
 # Replicate the text and formatting from the screenshot
 st.markdown("""
     ## Trivia Night by NCHS After Prom Team
     ###### Saturday, February 3, 2024 - 7 to 9 pm
     ###### Parke Regency, 1413 Lesley Dr, Bloomington - IL
-    ###### <span style="background-color:yellow; color:black; ">***Must be 21 or older to attend***</span> 
+    ###### <span style="background-color:yellow; color:black; ">***Must be 16 or older to attend***</span> 
 """, unsafe_allow_html=True)
 
 st.markdown("""
@@ -73,7 +101,7 @@ st.markdown("""
 
 st.markdown("""
     **Registration:**
-    Reserve your table by filling the form below. Payments can be made by cheeck, or using any payment methods below:
+    Reserve your table by filling the form below. Payments can be made by check, or using any payment methods below:
 """, unsafe_allow_html=True)
 venmourl = "https://venmo.com/u/Jayshri-Patel-5"
 recipient_id = 'Jayshri-Patel-5'
@@ -138,6 +166,13 @@ if submit_button:
     
     # Send the email
     send_email(team_contact_email, data)
+
+    #Save the sheet to Google Sheets
+
+    registration_data = [team_contact, team_name, team_contact_email, team_contact_phone]
+    save_to_sheet(registration_data, 'https://docs.google.com/spreadsheets/d/1dGjZj-QNGjpn-oGTkeYKuHfw-okNSM6iYh-HLEN255A/edit?usp=sharing')
+
+
     st.success("Registration submitted successfully! You will receive a confirmation email shortly.")
 
 # Run this in your terminal: streamlit run your_script.py
